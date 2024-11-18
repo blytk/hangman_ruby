724a30045f6222c57083cedfacfb9afde42c0f76
Yago <dolmayan@gmail.com>
2024-11-18 14:46:58 +0100

    Remove Player class, transfer logic to Game class instead

--- lib/Game.rb
+++ lib/Game.rb
@@ -3,52 +3,123 @@ require_relative "Player"
 require_relative "SecretWord"
 require_relative "Display"
 
-dictionary =  []
+class Game
+  attr_accessor :dictionary, :guess_number, :guesses_remaining, :letters_guessed, :last_guess, :secret_word
 
-# Load dictionary, words between 5-12 characters
-File.open("10000_word_dictionary.txt", "r") do |file|
-  lines = file.readlines
-  lines.each do |word|
-    if word.length >= 5 && word.length <= 12
-            dictionary.push(word.chomp)
-    end    
+  @@initial_number_of_guesses = 7
+  @@dictionary_file = "10000_word_dictionary.txt"
+
+  def initialize
+    @dictionary = []
+    @guess_number = 0
+    @guesses_remaining = @@initial_number_of_guesses
+    @letters_guessed = []
+    @last_guess = ""
+    @secret_word = nil
   end
-end
 
-def game_loop(dictionary)
-  game_over = false
-  # Create secret word to guess
-  secret_word = SecretWord.new(dictionary.sample)
-  ######### DEBUG
-  p secret_word.secret_word
-  ######## DEBUG
-  # Create player
-  player = Player.new
-  # While game is not over
-  until game_over == true
-    if player.guesses_remaining <= 0
-      game_over = true
-      Display.print_message("You are out of guesses, game is over")
-      break
+  # Load dictionary, words between 5-12 characters
+  def load_dictionary
+    File.open(@@dictionary_file, "r") do |file|
+      lines = file.readlines
+      lines.each do |word|
+        if word.length >= 5 && word.length <= 12
+          @dictionary.push(word.chomp.downcase)
+        end
+      end
     end
-    # Player makes a choice
-      # Display how many guesses so far (player.player_selects triggers this)
-      # Display how many guesses remaining (player.player_selects triggers this)
-      # # Display player selections so far
-    player.player_selects(secret_word)
-    # Display hangman
-    Drawing.draw_stick(player.guesses_remaining)
-    secret_word.print_word_status(player)
+  end
 
-    # Display word in current status (empty spaces, letters present in word)
+  def player_selects(secret_word)
+    input = ""
+    while true
+      check1 = false
+      check2 = false
 
+      Display.print_message("Enter your guess: ")
+      input = gets.chomp.downcase.chr
+
+      if input.match?(/\A[a-zA-Z]\z/)
+        check1 = true
+      else
+        Display.print_message("Invalid guess, try a letter")
+      end
+
+      # input should not have been already guessed
+      if letters_guessed.include?(input)
+        Display.print_message("That letter has already been tried. Try another letter.")
+        input = ""
+      else
+        check2 = true
+      end
+
+      if check1 == true && check2 == true
+        break
+      end
+
+    end
+    Display.print_message("You have selected: #{input}")
+    self.last_guess = input
+    self.letters_guessed.push(input)
+    self.print_guessed_letters  
+
+    add_guess_number
+
+    # if guess is not in secret word, we deduct 1 remaining guess
+    if !secret_word.secret_word.include?(self.last_guess)
+      deduct_guess_remaining
+    end
+
+    print_guess_status
+
+  end
+
+  def add_guess_number
+    self.guess_number += 1
+  end
+
+  def deduct_guess_remaining
+    self.guesses_remaining -= 1
+  end
+
+  def print_guess_status
+    Display.print_message("Guess # #{self.guess_number} | Guesses remaining: #{self.guesses_remaining}")
+  end
+
+  def print_guessed_letters
+    Display.print_message("The characters you have chosen so far are: #{@letters_guessed}")
   end
   
+
+  def game_loop
+    game_over = false
+    # Create secret word to guess
+    secret_word = SecretWord.new(@dictionary.sample)
+    ######### DEBUG
+    p secret_word.secret_word
+    ######## DEBUG
+    #
+    # While game is not over
+    until game_over == true
+      if @guesses_remaining <= 0
+        game_over = true
+        Display.print_message("You are out of guesses, game is over")
+        break
+      end
+      # Player makes a choice
+        # Display how many guesses so far (player.player_selects triggers this)
+        # Display how many guesses remaining (player.player_selects triggers this)
+        # # Display player selections so far
+      player_selects(secret_word)
+      # Display hangman
+      Drawing.draw_stick(player.guesses_remaining)
+      secret_word.print_word_status(player)
+
+      # Display word in current status (empty spaces, letters present in word)
+
+    end
+  end
 end
 
-game_loop(dictionary)
-
-
-
-
-
+game = Game.new
+game.game_loop
